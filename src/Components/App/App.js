34bfca55d19aa83a061b2b5dd496ed361c3599e1
Kagin007 <adamschulte148@hotmail.com>
2022-07-07 10:37:31 -0400

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        {/* <!-- Add a SearchBar component --> */}
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component -->
+          <!-- Add a Playlist component --> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
